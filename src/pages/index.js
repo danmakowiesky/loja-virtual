8a01949372500f18fcbf66ca49cb010745e4263b
danmakowiesky <dvm1993@hotmail.com>
2022-03-15 16:26:38 -0300

    main content

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,15 @@ export default function Home() {
   return (
     <>
       <Header />
-      <main className='main_content'>main</main>
+      <main className='main_content'>
+        <section className='pdt_gallery'>
+          <div className='container'>
+            <header className='main_content_header'>
+              <h1 className='main_content_header_title'>Ofertas da semana</h1>
+            </header>
+          </div>
+        </section>
+      </main>{' '}
       <footer className='main_footer'>footer</footer>
     </>
   );
